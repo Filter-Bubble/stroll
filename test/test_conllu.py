c357ac83891e134f544732c2875aaea781a3f777
Dafne van Kuppevelt <d.vankuppevelt@esciencecenter.nl>
2020-09-23 17:01:21 +0200

    Added simple test

---
+++ test/test_conllu.py
@@ -0,0 +1,19 @@
+import stroll.conllu
+import os
+
+__here__ = os.path.dirname(os.path.realpath(__file__))
+
+
+def test_empty_conll():
+    dataset = stroll.conllu.ConlluDataset()
+    assert len(dataset.sentences) == 0
+
+
+def test_load_conllu():
+    input_file = os.path.join(__here__, 'data', 'test.conllu')
+    dataset = stroll.conllu.ConlluDataset(input_file)
+    assert len(dataset.sentences) == 1
+    sent = dataset[0]
+    assert len(sent) == 11
+    tok = sent[0]
+    tok.COREF = '(0)'
