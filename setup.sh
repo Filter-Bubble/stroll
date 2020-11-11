dc372d9f0ddaa4e33d077a0b9684e01ad3c29949
Dafne van Kuppevelt <d.vankuppevelt@esciencecenter.nl>
2020-11-11 11:27:47 +0100

    also download fasttext

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 mkdir -p models
 
+# Download srl model
 if [ ! -f models/srl.pt ]; then
   wget https://surfdrive.surf.nl/files/index.php/s/kOgUm0oEpmx5HiZ/download -O models/srl.pt
+fi
+
+# Download word vectors
+if [ ! -f models/fasttext.model.bin ]; then
+  wget https://surfdrive.surf.nl/files/index.php/s/085yxFcRmn0osMw/download -O models/fasttext.model.bin
 fi
